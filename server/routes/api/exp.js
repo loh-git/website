3d81ce55b27932679f8081a98306412c1c03a158
Liam <ohanlonl@outlook.com>
2020-07-25 17:57:33 +0100

    Heroku URL Fix

--- server/routes/api/exp.js
+++ server/routes/api/exp.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', async (req,res) => {
 	var fs = require('fs');
 	var obj;
-	fs.readFile(__dirname + '../../json/exp.json', 'utf8', function(err, data) {
+	fs.readFile(__dirname + '/../json/exp.json', 'utf8', function(err, data) {
 		if (err) throw err;
 		obj = JSON.parse(data);
 		res.send(JSON.stringify(obj));
