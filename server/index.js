9f3ab5aa87e7452de7c6be7fb78159e8f3c4794c
Liam <ohanlonl@outlook.com>
2020-07-25 15:01:07 +0100

    Heroku Fix 2

--- server/index.js
+++ server/index.js
@@ -20,6 +20,14 @@ app.use('/api/exp',experience);
 app.use('/api/pers',personal);
 app.use('/api/skills',skills);
 
+// Production
+if(process.env.NODE_ENV === 'production') {
+	// Static folder
+	app.use(express.static(__dirname+'/public/'));
+	// SPA
+	app.get(/.*/, (req,res) => res.sendFile(__dirname+"/public/index.html"));
+}
+
 // Listen
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
